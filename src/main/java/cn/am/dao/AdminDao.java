810da5ebd5d1b7b53d60cedafb9d4409d3085c12
mtjmtj7 <mtjmtj7@outlook.com>
2019-05-27 18:26:07 +0800

    一些优化

--- src/main/java/cn/am/dao/AdminDao.java
+++ src/main/java/cn/am/dao/AdminDao.java
@@ -5,8 +5,27 @@ import cn.am.bean.State;
 import java.util.List;
 
 public interface AdminDao {
+    /**
+     * 查询所有待审批state
+     * @return
+     */
     public List<State> getAllStateNotPass();
+
+    /**
+     * 查询所有已审批state
+     * @return
+     */
     public List<State> getAllPassState();
+
+    /**
+     * 查询所有不足15天审批state
+     * @return
+     */
     public List<State> getAllNearEndState();
+
+    /**
+     * 查询所有过期审批state
+     * @return
+     */
     public List<State> getAllEndState();
 }
--- src/main/java/cn/am/dao/UserDao.java
+++ src/main/java/cn/am/dao/UserDao.java
@@ -7,11 +7,46 @@ import cn.am.bean.User;
 import java.util.List;
 
 public interface UserDao {
+    /**
+     * 登录
+     * @param user
+     * @return
+     */
     public User login(User user);
+
+    /**
+     * 查询所有状态
+     * @param uid
+     * @return
+     */
     public List<State> getAllState(String uid);
+
+    /**
+     * 动态更新state
+     * @param state
+     * @return
+     */
     public int updateState(State state);
+
+    /**
+     * 动态更新apply
+     * @param apply
+     * @return
+     */
     public int updateApply(Apply apply);
+
+    /**
+     * 按ID查询apply
+     * @param aid
+     * @return
+     */
     public Apply selectApplyByID(String aid);
+
+    /**
+     * 按ID查询state
+     * @param sid
+     * @return
+     */
     public State selectStateByID(String sid);
     /**
      * 查询已经被授权记录
